d119d958b6e86b099bc97d8690e1576cb5ea4970
StasyaV <46928311+StasyaV@users.noreply.github.com>
2020-03-12 12:20:26 +0300

    Открытие-закрытие формы

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 var searchButton = document.querySelector(".find-hotel__button");
 var searchForm = document.querySelector(".search");
-var open = searchForm.querySelector(".search-form-close");
 var checkIn = searchForm.querySelector("[name=check-in-date]");
 var checkOut = searchForm.querySelector("[name=check-out-date]");
 
-  searchButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
+searchButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (searchForm.classList.contains("search-form-open")) {
+    searchForm.classList.remove("search-form-open");
     searchForm.classList.add("search-form-close");
-  });
-  searchButton.addEventListener("click", function (evt) {
+  } else {
+      searchButton.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        searchForm.classList.remove("search-form-close");
+        searchForm.classList.add("search-form-open");
+      });
+  }
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
     evt.preventDefault();
-    searchButton.classList.add("open");
-  });
-  open.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    searchForm.classList.remove("search-form-close");
-    checkIn.focus();
-  });
+    if (searchForm.classList.contains("search")) {
+      searchForm.classList.add("search-form-close");
+      searchForm.classList.remove("search-form-error");
+    }
+  }
+});
   searchForm.addEventListener("submit", function (evt) {
     if (!checkIn.value || !checkOut.value) {
       evt.preventDefault();
+      searchForm.classList.remove("search-form-error");
+      searchForm.offsetWidth = searchForm.offsetWidth;
+      searchForm.classList.add("search-form-error");
       console.log("Нужно заполнить даты приезда и отъезда");
-    }
-  });
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (searchForm.classList.contains("search")) {
-        searchForm.classList.add("search-form-close");
-      }
     }
   });
